9a8aac4df02690ae69d41561409934dc4817ada6
ilya <ilya.dyachenko@gmail.com>
2015-08-29 23:31:08 +0300

    backup

--- www/js/script.js
+++ www/js/script.js
@@ -1,67 +1,63 @@
 'use strict';
 
-var data = {
-    1: {name: "Mark", lastName: "Otto", userName: "Otto"},
-    2: {name: "Jacob", lastName: "Thornton", userName: "@fat"},
-    3: {name: "Larry", lastName: "the Bird", userName: "@twitter"}
-};
 
 var MainCheckBox = React.createClass({
-    handleChange: function () {
-        if (event.target.checked) {
-
-        }
-    },
     render: function () {
-        return (<input className="multi-select" onChange={this.handleChange} type="checkbox"/>)
+        return (<input className="multi-select" onChange={this.props.onChange} type="checkbox"/>)
     }
 });
 
-React.render(
-    <MainCheckBox />,
-    document.getElementById('mainCheckBox')
-);
-
-
 var ShowBtn = React.createClass({
     clickHandler: function () {
-        debugger
+        var _arr = [];
+        for (var i in this.props.data) {
+            if (this.props.data[i].checked) {
+                _arr.push(i);
+            }
+        }
+        console.log(_arr);
     },
     render: function () {
         return (<button className="btn btn-default action-button" type="submit" onClick={this.clickHandler}>Show selected id's</button>)
     }
 });
 
-React.render(
-    <ShowBtn />,
-    document.getElementById('showBtn')
-);
-
 var TableRow = React.createClass({
+    handleChange: function () {
+        if (event.target.checked) {
+            this.props.data.checked = true;
+        }
+        else {
+            this.props.data.checked = false;
+        }
+
+        this.setState({});
+    },
     render: function () {
+        var i = this.props.value
         return (
             <tr>
                 <th scope="row">
-                    <input type="checkbox" value={this.props.value} />
+                    <input type="checkbox" value={i} checked={this.props.data.checked} onChange={this.handleChange} />
                 </th>
-                <td>{this.props.name}</td>
-                <td>{this.props.lastName}</td>
-                <td>{this.props.userName}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.lastName}</td>
+                <td>{this.props.data.userName}</td>
             </tr>
         )
     }
 });
 
 var TableList = React.createClass({
-    handleChange: function () {
-        console.log(this);
-    },
     render: function () {
         var rows = [];
 
+        var data = this.props.data;
+
         for (var i in data) {
-            var _valObj = data[i];
-            rows.push(<TableRow name={_valObj.name}  lastName={_valObj.lastName}  userName={_valObj.userName} value={i} />);
+            rows.push(<TableRow
+                data={data[i]}
+                value={i} />);
         }
 
         return (
@@ -82,8 +78,46 @@ var TableList = React.createClass({
     }
 });
 
+var MyContainer = React.createClass({
+    getInitialState: function () {
+        return {
+            data: {
+                1: {name: "Mark", lastName: "Otto", userName: "Otto", checked: false},
+                2: {name: "Jacob", lastName: "Thornton", userName: "@fat", checked: false},
+                3: {name: "Larry", lastName: "the Bird", userName: "@twitter", checked: false}
+            }
+        };
+    },
+    handleChangeMainCheckBox: function () {
+        if (event.target.checked) {
+            for (var i in this.state.data) {
+                this.state.data[i].checked = true;
+            }
+        }
+        else {
+            for (var i in this.state.data) {
+                this.state.data[i].checked = false;
+            }
+        }
+
+        this.setState({});
+    },
+    render: function () {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <MainCheckBox onChange={this.handleChangeMainCheckBox} />
+                    <ShowBtn data={this.state.data} />
+                </div>
+                <div className="panel-body">
+                    <TableList data={this.state.data} />
+                </div>
+            </div>
+        )
+    }
+});
+
 React.render(
-    <TableList />
-    ,
-    document.getElementById('tableList')
+    <MyContainer />,
+    document.getElementById('myContainer')
 );
